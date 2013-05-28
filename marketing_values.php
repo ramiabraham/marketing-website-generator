9df90078f6f3a7190e7bc33c037e57e141c02f3a
Rami Abraham <r@radesign.ws>
2013-05-28 02:43:02 -0400

    Added more terms to arrays, added comment

--- marketing_values.php
+++ marketing_values.php
@@ -1,13 +1,20 @@
-<?php $logo_one_items = Array('HYPER','COMPU','MEGA','ULTRA','GLOBAL','SELL','MAXI','NET','SUPER','CLICK','WEB','SOCIAL','BUILD','BOOST','HERO','INTRA','RAD');
+// Not much going on here, just the dack.com arrays with a few more terms from me thrown in.
 
-$logo_two_items = Array('COMM','LEADS','BLAST','SERGY','GLOBAL','GENIUS','SOCIAL','BOOST','NET','COM','ZAZZ','POP','SWELL','BEAST','FLOW','MONEYS','BONE');  
+<?php $logo_one_items = Array('HYPER','COMPU','MEGA','DATA','WEB','WEB2','FACE','FOCUS','PERFORMA','ACCU','DOUBLE','LIKE','SHARE','WIDGET','TOUCH','APP','BESTY','VIRAL','MARKET','ULTRA','GLOBAL','SELL','MAXI','NET','SUPER','CLICK','WEB','SOCIAL','BUILD','BOOST','HERO','INTRA','RAD');
 
-$global_leader_in = Array('ROI','Web-Alytics','market-driven cross-synergy','socially-empowered e-tailing','synergy mediaries',);
+$logo_two_items = Array('COMM','LEADS','BLAST','APP','SURGE','SUITE','NINJAS','LOLZ','PERFORMER','BOOSTER','MASTER','MAX','GRIP','GLEAM','BOOM','SQUEEZE','NUT','BEACON','GRAM','LOVE','DEMON','ANGEL','GLOBAL','GENIUS','SOCIAL','BOOST','NET','COM','ZAZZ','POP','SWELL','BEAST','FLOW','MONEYS','BONE');  
 
-$bs_number_one = Array('thousands','millions','billions','trillions','quadrillions','bajillions');
+$global_leader_in = Array('ROI','Web-Alytics','market-driven cross-synergy','socially-empowered e-tailing','synergy mediaries',"synergies", "web-readiness", "paradigms", "markets", "partnerships", "infrastructures", "platforms", 
+"initiatives", "channels", "eyeballs", "communities", "ROI", "solutions", "e-tailers", "e-services", "action-items", 
+"portals", "niches", "technologies", "content", "vortals", "supply-chains", "convergence", "relationships", 
+"architectures", "interfaces", "e-markets", "e-commerce", "systems", "bandwidth", "infomediaries", "models", 
+"mindshare", "deliverables", "users", "schemas", "networks", "applications", "metrics", "e-business", "functionalities", 
+"experiences", "web services", "methodologies");
+
+$bs_number_one = Array('lots','a crap-ton','hundreds','a whole bunch','thousands','millions','billions','trillions','quadrillions','bajillions');
 
 
-$bs_number_two = Array('second','hour','day','week','month');
+$bs_number_two = Array('femto-second','nano-second','mill-isecond','second','hour','day','week','month','cmapaign','business day','fiscal year','quarter');
 
 $header_banner_year = rand(10, 99);;
 
@@ -36,6 +43,4 @@ $header_banner_noun_one = Array("synergies", "web-readiness", "paradigms", "mark
 "architectures", "interfaces", "e-markets", "e-commerce", "systems", "bandwidth", "infomediaries", "models", 
 "mindshare", "deliverables", "users", "schemas", "networks", "applications", "metrics", "e-business", "functionalities", 
 "experiences", "web services", "methodologies")
-
-
 ?>
